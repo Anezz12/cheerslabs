7dd1f4f7f7358705b744bba5fae323c9cca9d8d9
Gabriel Bayu <gabrielbayu9@gmail.com>
2025-06-23 23:44:33 +0700

    tambah foto + update page menu

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function AboutusPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold mb-8">About Us</h1>
+            <p className="text-lg text-center max-w-2xl mb-8">
+                Cheers Labs
+            </p>
+        </div>
+    );
+    }
